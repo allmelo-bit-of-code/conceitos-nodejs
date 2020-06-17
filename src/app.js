9e47c1e7789e2088e07508de5bdac6f6f1f401f6
Allan Melo <allmelo@users.noreply.github.com>
2020-06-17 20:55:19 -0300

    Get, post & put working

--- src/app.js
+++ src/app.js
@@ -10,6 +10,37 @@ app.use(cors());
 
 const repositories = [];
 
+class Repo {
+  constructor(title, url, techs) {
+    this.title = title,
+    this.url = url,
+    this.techs = techs,
+    this.id = uuid(),
+    this.likes = 0
+  };
+
+  like() {
+    this.like += 1;
+  };
+
+};
+
+function idStatusOnRepo(reqParam){
+  const { id } = reqParam;
+
+  if (!isUuid(id)){
+    return response.status(400).json(`error: uuid ${id} is invalid`)
+  }; 
+  
+  const paramIndex = repositories.findIndex( repo => repo.id === id );
+  
+  if (repoIndex < 0) {
+    return response.status(400).json(notFound(`error: uuid ${id} is not found`))
+  } else {
+    return paramIndex
+  }
+};
+
 app.get("/repositories", (request, response) => {
   // TODO
 });
