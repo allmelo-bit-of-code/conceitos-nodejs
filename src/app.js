0226eebae13ead6076a930c63220ef61ed782f91
Allan Melo <allmelo@users.noreply.github.com>
2020-06-17 20:56:10 -0300

    Routes (1/2) working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
+const { Route } = require("express");
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
@@ -29,28 +28,46 @@ function idStatusOnRepo(reqParam){
   const { id } = reqParam;
 
   if (!isUuid(id)){
-    return response.status(400).json(`error: uuid ${id} is invalid`)
-  }; 
-  
-  const paramIndex = repositories.findIndex( repo => repo.id === id );
-  
+    response.status(400).json(`error: uuid ${id} is invalid`);
+    return false;
+  };
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id );
+
   if (repoIndex < 0) {
-    return response.status(400).json(notFound(`error: uuid ${id} is not found`))
+    response.status(400).json(notFound(`error: uuid ${id} is not found`))
+    return false;
   } else {
-    return paramIndex
+    return repoIndex;
   }
 };
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepo = new Repo(title, url, techs);
+  repositories.push(newRepo);
+  return response.json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const repoIndex = idStatusOnRepo(request.params);
+
+  if (!repoIndex){
+    return console.error('not alowed');
+
+  } else {
+    const { title, url, techs } = request.body;
+    
+    title ? repositories[repoIndex].title = title : '';
+    url ? repositories[repoIndex].url = url : '';
+    techs ? repositories[repoIndex].techs = techs : '';        
+    
+    return response.json(repositories[repoIndex])
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
